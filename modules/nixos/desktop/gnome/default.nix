15dcc3aed312d8b09a48cd1987e9665e9f5ba757
= <=>
2024-10-31 11:41:28 +0000

    Updated to nividia beta driver and added some GNOME app exclusions.

--- modules/nixos/desktop/gnome/default.nix
+++ modules/nixos/desktop/gnome/default.nix
@@ -1,26 +1,33 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-  # Additional metadata is provided by Snowfall Lib.
-  namespace, # The namespace used for your flake, defaulting to "internal" if not set.
-  system, # The system architecture for this host (eg. `x86_64-linux`).
-  target, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format, # A normalized name for the system target (eg. `iso`).
-  virtual, # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems, # An attribute map of your defined hosts.
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  inputs
+, # Additional metadata is provided by Snowfall Lib.
+  namespace
+, # The namespace used for your flake, defaulting to "internal" if not set.
+  system
+, # The system architecture for this host (eg. `x86_64-linux`).
+  target
+, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format
+, # A normalized name for the system target (eg. `iso`).
+  virtual
+, # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems
+, # An attribute map of your defined hosts.
   # All other arguments come from the module system.
-  config,
-  ...
+  config
+, ...
 }:
 with lib;
 with lib.nix-config; let
   cfg = config.desktop.gnome;
-in {
+in
+{
   options.desktop.gnome = {
     enable = mkEnableOption "Enable GNOME desktop";
   };
@@ -29,13 +36,31 @@ in {
     services.libinput.enable = true;
     # To fix GTK apps:
     programs.dconf.enable = true;
+    services.xserver.enable = true;
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
+    environment.gnome.excludePackages = (with pkgs; [
+      gnome-photos
+      gnome-tour
+      gedit
+      cheese # webcam tool
+      gnome-music
+       # text editor
+      epiphany # web browser
+      geary # email reader
+      gnome-characters
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+      atomix # puzzle game
+      yelp # Help view
+      gnome-contacts
+      gnome-initial-setup      
+    ]);
 
-    services.xserver = {
-      enable = true;
-      displayManager.gdm = {
-        enable = true;
-      };
-      desktopManager.gnome.enable = true;
-    };
+    environment.systemPackages = with pkgs; [
+      gnome-tweaks
+    ];
+
   };
 }
--- modules/nixos/hardware/nvidia/default.nix
+++ modules/nixos/hardware/nvidia/default.nix
@@ -1,49 +1,82 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-  # Additional metadata is provided by Snowfall Lib.
-  namespace, # The namespace used for your flake, defaulting to "internal" if not set.
-  system, # The system architecture for this host (eg. `x86_64-linux`).
-  target, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format, # A normalized name for the system target (eg. `iso`).
-  virtual, # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems, # An attribute map of your defined hosts.
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  inputs
+, # Additional metadata is provided by Snowfall Lib.
+  namespace
+, # The namespace used for your flake, defaulting to "internal" if not set.
+  system
+, # The system architecture for this host (eg. `x86_64-linux`).
+  target
+, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format
+, # A normalized name for the system target (eg. `iso`).
+  virtual
+, # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems
+, # An attribute map of your defined hosts.
   # All other arguments come from the module system.
-  config,
-  ...
+  config
+, ...
 }:
 with lib;
 with lib.nix-config; let
   cfg = config.hardware.nvidia;
-in {
+in
+{
   options.hardware.nvidia = with types; {
     enable = mkOpt types.bool true "Enable nvidia config";
   };
 
   config = mkIf cfg.enable {
-    hardware.nvidia = {
-      open = false;
-      modesetting.enable = true;
-      nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    services.xserver.videoDrivers = [ "nvidia" ];
+    nixpkgs.config.allowUnfree = true;
+
+    hardware = {
+      graphics = {
+        enable = true;
+        enable32Bit = true;
+        extraPackages = with pkgs; [ libva vaapiVdpau ];
+      };
     };
 
-    services.xserver.videoDrivers = ["nvidia"];
+    nixpkgs.config.nvidia.acceptLicense = true;
+    hardware.nvidia = {
 
-    environment.systemPackages = with pkgs; [
-      cudatoolkit
-      cudaPackages.libcublas
-      cudaPackages.libcufft
-      cudaPackages.libcurand
-      cudaPackages.libcusparse
-      cudaPackages.libcusolver
-      cudaPackages.cuda_nvrtc
-      cudaPackages.cuda_nvprof
-    ];
+      # Modesetting is required.
+      modesetting.enable = true;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+      # of just the bare essentials.
+      powerManagement.enable = true;
+
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of 
+      # supported GPUs is at: 
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
+      open = false;
+
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      # .latest or .beta for options here.
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
+    };
   };
 }
